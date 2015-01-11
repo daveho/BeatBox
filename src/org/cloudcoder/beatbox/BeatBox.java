0bea44eb2cc9aa0d4b5ba29191cc8d488e6617f7
David Hovemeyer <david.hovemeyer@gmail.com>
2015-01-11 09:31:44 -0500

    removed some unused code

--- src/org/cloudcoder/beatbox/BeatBox.java
+++ src/org/cloudcoder/beatbox/BeatBox.java
@@ -58,50 +58,6 @@ public class BeatBox {
 		}
 		return g;
 	}
-
-	/*
-	public void kicksAndClaps() {
-		SequencerEvent kick = new PlaySampleEvent(Samples.KICK_1, 0.4f);
-		SequencerEvent hihat1 = new PlaySampleEvent(Samples.HIHAT_1, 0.4f);
-		
-		EventGroup g = group(
-				0, kick,
-				8, kick,
-				0, hihat1,
-				4, hihat1,
-				8, hihat1,
-				12, hihat1
-		);
-
-		// Play the pattern for 10 measures
-		for (int i = 0; i < 10; i++) {
-			g.schedule(i*BPM, seq);
-		}
-		
-		SequencerEvent kick = new PlaySampleEvent(Samples.KICK_1, 0.4f);
-		seq.repeat(0, kick);
-		seq.repeat(BPM/2, kick);
-		
-//		SequencerEvent clap1 = new PlaySampleEvent(Samples.CLAP_1, 0.4f);
-//		SequencerEvent clap3 = new PlaySampleEvent(Samples.CLAP_1, 0.4f);
-		
-//		seq.repeat(3, clap1);
-//		seq.repeat(6, clap1);
-//		seq.repeat(BPM/2+2, clap3);
-//		seq.repeat(BPM/2+4, clap3);
-		
-		
-		SequencerEvent hihat1 = new PlaySampleEvent(Samples.HIHAT_1, 0.4f);
-		
-//		seq.repeat(2, hihat1);
-//		seq.repeat(3, hihat1);
-//		seq.repeat(10, hihat1);
-//		seq.repeat(11, hihat1);
-		for (int i = 0; i < BPM; i += 2) {
-			seq.repeat(i, hihat1);
-		}
-	}
-		*/
 	
 	public void addKicks() {
 		for (int i = 0; i < 10; i++) {
