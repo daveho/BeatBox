753bc4b55d7f886ccc8cf9caa70cf8320836c371
David Hovemeyer <david.hovemeyer@gmail.com>
2015-01-17 16:48:40 -0500

    mix up the hi-hat sounds a bit

--- src/org/cloudcoder/beatbox/BeatBox.java
+++ src/org/cloudcoder/beatbox/BeatBox.java
@@ -7,6 +7,7 @@ public class BeatBox extends Player {
 
 	private PlaySampleEvent kick;
 	private PlaySampleEvent hihat1;
+	private PlaySampleEvent hihat2;
 	private PlaySampleEvent snare1;
 	
 	public BeatBox() {
@@ -14,6 +15,7 @@ public class BeatBox extends Player {
 		Samples.loadAll();
 		kick = new PlaySampleEvent(Samples.KICK_1, 0.4f);
 		hihat1 = new PlaySampleEvent(Samples.HIHAT_1, 0.3f);
+		hihat2 = new PlaySampleEvent(Samples.HIHAT_2, 0.3f);
 		snare1 = new PlaySampleEvent(Samples.SNARE_1, 0.5f);
 	}
 	
@@ -45,6 +47,19 @@ public class BeatBox extends Player {
 				);
 	}
 	
+	private EventGroup hihats2() {
+		return group(
+				0, hihat1,
+				2, hihat1,
+				4, hihat2,
+				6, hihat2,
+				8, hihat1,
+				10, hihat2,
+				12, hihat1,
+				14, hihat2
+				);
+	}
+	
 	private EventGroup snare1() {
 		return group(8, snare1, 12, snare1, 14, snare1);
 	}
@@ -64,10 +79,21 @@ public class BeatBox extends Player {
 		return m+12;
 	}
 	
-	private int addRhythm2(int m) {
+	private int addBasicKicks(int m) {
 		int start = m*BPM;
 		seq.atBeats(start+0, 4, BPM, kicks2());
 		return m+4;
+	}
+	
+	private int addRhythm3(int m) {
+		int start = m*BPM;
+		seq.atBeats(start, 12, BPM, kicks());
+		seq.atBeats(start+4*BPM, 8, BPM, hihats2());
+		seq.atBeats(start+4*BPM, 2, BPM, snare1());
+		seq.atBeats(start+6*BPM, 2, BPM, snare2());
+		seq.atBeats(start+8*BPM, 2, BPM, snare1());
+		seq.atBeats(start+10*BPM, 2, BPM, snare2());
+		return m+12;
 	}
 	
 	public static void main(String[] args) {
@@ -75,11 +101,10 @@ public class BeatBox extends Player {
 
 		int m = 0;
 		
-		m = beatBox.addRhythm2(m);
+		m = beatBox.addBasicKicks(m);
 		m = beatBox.addRhythm(m);
-		m = beatBox.addRhythm2(m);
-		m = beatBox.addRhythm(m);
-		m = beatBox.addRhythm2(m);
+		m = beatBox.addBasicKicks(m);
+		m = beatBox.addRhythm3(m);
 		
 		beatBox.play();
 	}
