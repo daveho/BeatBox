05c3d0dcf58261c08150aa2e100f75edc11a5ce7
David Hovemeyer <david.hovemeyer@gmail.com>
2015-01-23 16:23:09 -0500

    Capture MidiEvents!

---
+++ src/io/github/daveho/beatbox/CaptureMidiEvents.java
@@ -0,0 +1,58 @@
+package io.github.daveho.beatbox;
+
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.Transmitter;
+
+public class CaptureMidiEvents {
+	
+	public static void getMidiInput(Receiver receiver) throws MidiUnavailableException {
+		
+		MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+		for (MidiDevice.Info info : infos) {
+			MidiDevice device;
+			device = MidiSystem.getMidiDevice(info);
+			System.out.println("Found: " + device);
+			
+			int maxTransmitters = device.getMaxTransmitters();
+			System.out.println("  Max transmitters: " + maxTransmitters);
+			
+			if (maxTransmitters == -1 || maxTransmitters > 0) {
+				Transmitter transmitter = device.getTransmitter();
+				transmitter.setReceiver(receiver);
+				device.open();
+				return;
+			}
+		}
+		
+		throw new MidiUnavailableException("Could not find any midi input sources");
+	}
+	
+	static class MyReceiver implements Receiver {
+
+		@Override
+		public void send(MidiMessage message, long timeStamp) {
+			System.out.println("Recieved MidiMessage@" + timeStamp);
+		}
+
+		@Override
+		public void close() {
+			System.out.println("Closing...");
+		}
+	}
+	
+	public static void main(String[] args) throws MidiUnavailableException {
+		getMidiInput(new MyReceiver());
+		
+		while (true) {
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				System.out.println("Interrupted?");
+			}
+		}
+	}
+}
