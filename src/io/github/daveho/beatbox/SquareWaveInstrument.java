a50037ff745770bb5e54d4aee417eae865efd02e
David Hovemeyer <david.hovemeyer@gmail.com>
2015-02-01 18:37:09 -0500

    figured out why note gain wasn't working

--- src/io/github/daveho/beatbox/SquareWaveInstrument.java
+++ src/io/github/daveho/beatbox/SquareWaveInstrument.java
@@ -18,14 +18,17 @@ public class SquareWaveInstrument extends Bead /*extends UGenChain*/ {
 	private Builder b;
 
 	public SquareWaveInstrument(AudioContext ac, float gain) {
-		this.out = new Gain(ac, 1, gain);
+		this.out = new Gain(ac, 1, 0.15f);
 		
 		this.freq = new Static(ac, 0f);
 		
+		
 		this.b = Builder.build(ac, out)
-				.prepend(new LinearRise(ac, 120.0f)).label("rise")
+//				.prepend(new LinearRise(ac, 120.0f)).label("rise")
 				.prepend(new Gain(ac, 1, 0f)).label("noteGain")
 				.prepend(new WavePlayer(ac, freq, Buffer.SQUARE)).label("waveplayer");
+		
+//		System.out.println("ng.getValue() = " + ng.getValue());
 		
 		out.pause(true);
 	}
@@ -53,6 +56,7 @@ public class SquareWaveInstrument extends Bead /*extends UGenChain*/ {
 			
 			UGen noteGainUGen = b.get("noteGain");
 			System.out.println("noteGain is a " + noteGainUGen.getClass().getSimpleName());
+			System.out.println("  Its value is " + noteGainUGen.getValue());
 			
 			if (msg.getStatus() == Midi.STATUS_KEY_DOWN) {
 				// Enable output
@@ -61,7 +65,7 @@ public class SquareWaveInstrument extends Bead /*extends UGenChain*/ {
 				// Start playing
 				float noteGain = Midi.getVelocity(msg) / 128.0f;
 //				System.out.println("Note gain is " + noteGain);
-				noteGainUGen.setValue(noteGain);
+				((Gain)noteGainUGen).setGain(noteGain);
 				freq.setValue(Pitch.mtof(Midi.getNote(msg)));
 			} else if (msg.getStatus() == Midi.STATUS_KEY_UP) {
 				// Stop playing
