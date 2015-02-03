637327dd2c0983a626e1746b757b30c77d65eca2
David Hovemeyer <david.hovemeyer@gmail.com>
2015-02-02 19:18:01 -0500

    factored out WaveInstrument from SquareWaveInstrument

--- src/io/github/daveho/beatbox/SquareWaveInstrument.java
+++ src/io/github/daveho/beatbox/SquareWaveInstrument.java
@@ -1,27 +1,13 @@
 package io.github.daveho.beatbox;
 
-import javax.sound.midi.MidiMessage;
-
 import net.beadsproject.beads.core.AudioContext;
-import net.beadsproject.beads.core.Bead;
-import net.beadsproject.beads.core.UGen;
-import net.beadsproject.beads.core.UGenChain;
 import net.beadsproject.beads.data.Buffer;
-import net.beadsproject.beads.data.Pitch;
-import net.beadsproject.beads.ugens.Gain;
-import net.beadsproject.beads.ugens.Static;
-import net.beadsproject.beads.ugens.WavePlayer;
 
 /**
  * An instrument which listens for MidiMessages and plays square
  * waves in response.
  */
-public class SquareWaveInstrument extends UGenChain {
-
-	private Gain out;
-	private float gain;
-	private Static freq;
-	private Builder b;
+public class SquareWaveInstrument extends WaveInstrument {
 
 	/**
 	 * Constructor.
@@ -30,47 +16,6 @@ public class SquareWaveInstrument extends UGenChain {
 	 * @param gain  the maximum gain
 	 */
 	public SquareWaveInstrument(AudioContext ac, float gain) {
-		super(ac, 1, 1);
-		this.out = new Gain(ac, 1);
-		this.gain = gain;
-		
-		this.freq = new Static(ac, 0f);
-		
-		this.b = Builder.build(ac, out)
-				.prepend(new NoteOnLinearFadeIn(ac, 120.0f)).label("rise")
-				.prepend(new Gain(ac, 1, 0f)).label("noteGain")
-				.prepend(new WavePlayer(ac, freq, Buffer.SQUARE)).label("waveplayer");
-		
-		addToChainOutput(out);
-	}
-	
-	@Override
-	protected void messageReceived(Bead message) {
-		if (Midi.hasMidiMessage(message)) {
-			// Broadcast the message to the other UGens in the signal chain
-			for (UGen ugen : b.all()) {
-				System.out.println("Notify " + ugen.getClass().getSimpleName());
-				ugen.message(message);
-			}
-			
-			MidiMessage msg = Midi.getMidiMessage(message);
-			
-			UGen noteGainUGen = b.get("noteGain");
-//			System.out.println("noteGain is a " + noteGainUGen.getClass().getSimpleName());
-//			System.out.println("  Its value is " + noteGainUGen.getValue());
-			
-			if (msg.getStatus() == Midi.STATUS_KEY_DOWN) {
-				// Start playing
-				float noteGain = (Midi.getVelocity(msg) / 128.0f) * gain;
-//				System.out.println("Note gain is " + noteGain);
-				((Gain)noteGainUGen).setGain(noteGain);
-				freq.setValue(Pitch.mtof(Midi.getNote(msg)));
-			} else if (msg.getStatus() == Midi.STATUS_KEY_UP) {
-				// Stop playing
-				// TODO: could set a trigger for a dynamic fade, for now just stop abruptly
-				freq.setValue(0f);
-				noteGainUGen.setValue(0f);
-			}
-		}
+		super(ac, Buffer.SQUARE, gain);
 	}
 }
